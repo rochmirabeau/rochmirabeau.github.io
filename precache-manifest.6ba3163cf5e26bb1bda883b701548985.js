d21b8515066caa0be29c18fd6805948d022b7163
Roch X20W <32148092+rochmirabeau@users.noreply.github.com>
2019-11-18 01:29:17 -0700

    Updates

---
+++ precache-manifest.6ba3163cf5e26bb1bda883b701548985.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf6ced518e911a617912663bdcd50bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d5569934b8e5174dfdc",
+    "url": "/static/css/main.643df0e2.chunk.css"
+  },
+  {
+    "revision": "e2da6739471abb718964",
+    "url": "/static/js/2.db906fa6.chunk.js"
+  },
+  {
+    "revision": "8d5569934b8e5174dfdc",
+    "url": "/static/js/main.6dfdd820.chunk.js"
+  },
+  {
+    "revision": "0d991a80c79c5e75fcdb",
+    "url": "/static/js/runtime-main.9d07dc90.js"
+  },
+  {
+    "revision": "fb4050ccd71fa904b75966f2737a0cd9",
+    "url": "/static/media/Background.fb4050cc.jpeg"
+  },
+  {
+    "revision": "51f5c057ecc2a2104ad0dcabe05e8987",
+    "url": "/static/media/GitHub-Mark.51f5c057.png"
+  },
+  {
+    "revision": "30d2c2a3bbd90b84f6357e0612b94f25",
+    "url": "/static/media/email300.30d2c2a3.png"
+  },
+  {
+    "revision": "7128d5991d5ba6626b053d7d974656ec",
+    "url": "/static/media/shftr.7128d599.png"
+  },
+  {
+    "revision": "f45a9d57f935dd646d2be30165005578",
+    "url": "/static/media/skilltrade.f45a9d57.png"
+  }
+]);
